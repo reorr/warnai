441fac78946789342680d5409bfac57a038c7a6e
reorr <hasbeeazam@gmail.com>
2018-11-12 21:32:11 +0000

    fix gtk assets not rendered properly

--- gtk/fantome/gtk-3.0/render-assets.sh
+++ gtk/fantome/gtk-3.0/render-assets.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
@@ -6,15 +6,15 @@ OPTIPNG="/usr/bin/optipng"
 SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
 INDEX="assets.txt"
+echo "Rendering gtk3 assets"
 
-for i in `cat $INDEX`
-do 
-    echo Rendering $ASSETS_DIR/$i.png
+for i in `cat $INDEX`; do 
+    echo "Rendering $ASSETS_DIR/$i.png"
     $INKSCAPE --export-id=$i \
               --export-id-only \
               --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
     && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
-    echo Rendering $ASSETS_DIR/$i@2.png
+    echo "Rendering $ASSETS_DIR/$i@2.png"
     $INKSCAPE --export-id=$i \
               --export-dpi=180 \
               --export-id-only \
